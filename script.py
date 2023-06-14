c3595a0b717c6784aa87b53adf9bf68a346ef0f9
Johnny Klee <john_klee@hotmail.com>
2023-06-14 13:02:44 +0200

    added test objects

---
+++ script.py
@@ -0,0 +1,5 @@
+# this is a recommendation system as a project for codecademy
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China',['historical site', 'art']]
